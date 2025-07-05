9a00ee0c63dbd4565d2b504d1f1fcd5f47c2b1c3
kcb1991 <kboat@byupathway.edu>
2025-07-04 17:38:33 -0700

    commit

---
+++ week02/scripts/temples.js
@@ -0,0 +1,5 @@
+const yearSpan = document.getElementById("currentyear");
+yearSpan.textContent = new Date().getFullYear();
+
+const modifiedSpan = document.getElementById("lastModified");
+modifiedSpan.textContent = `Last Modified: ${document.lastModified}`;
