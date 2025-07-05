d6265261bcd71519ef9ac566eb569cd23cd84aa9
kcb1991 <kboat@byupathway.edu>
2025-07-05 15:57:10 -0700

    week2 picture album

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -4,3 +4,12 @@ yearSpan.textContent = new Date().getFullYear();
 const modifiedSpan = document.getElementById("lastModified");
 modifiedSpan.textContent = `Last Modified: ${document.lastModified}`;
 
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerBtn = document.getElementById("hamburger");
+    const nav = document.querySelector("nav");
+
+    hamburgerBtn.addEventListener("click", () => {
+        nav.classList.toggle("active");
+        hamburgerBtn.textContent = nav.classList.contains("active") ? "❌" : "☰";
+    });
+});
